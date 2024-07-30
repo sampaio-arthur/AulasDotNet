92c4c60cd5534bdee2d0b176f1c202c94ed0c3b0
sampaio-arthur <arthurbarrasampaio@gmail.com>
2024-07-30 13:56:28 -0300

    Mais uma aula

---
+++ Poo/ContaBancaria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Poo
+{	
+	class ContaBancaria
+	{
+		public string titular;
+		public int numConta;
+		public double saldo;
+
+		public void Sacar(double valor)
+		{
+			saldo -= valor;
+		}
+
+		public void Depositar(double valor)
+		{
+			saldo += valor;
+		}
+		
+		public void Transferir(double valor, ContaBancaria contaDestino)
+		{
+			Sacar(valor);
+			contaDestino.Depositar(valor);
+		}
+	}
+
+}
---
+++ Poo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace Poo
+{
+    class Program
+    {
+        static void Main()
+        {
+            ContaBancaria c1 = new ContaBancaria();
+            ContaBancaria c2 = new ContaBancaria();
+
+            c1.numConta = 4567;
+            c2.numConta = 8900;
+            c1.titular = "Arthur";
+            c2.titular = "Matheus";
+            
+            c1.Depositar(200);
+            c1.Depositar(50);
+            c1.Sacar(150);
+
+            c2.Depositar(1000);
+            c2.Sacar(100);
+
+            c2.Transferir(200, c1);
+
+            Console.WriteLine(c1.numConta + "=>" + c1.saldo);
+            Console.WriteLine(c2.numConta + "=>" + c2.saldo);
+            Console.ReadLine();
+        }
+    }
+}
+
+    
